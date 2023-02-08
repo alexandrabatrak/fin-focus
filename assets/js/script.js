83e430cee658d85b3a12c76d79da00a2eda85353
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-02-08 21:17:27 +0000

    Initial API tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,55 @@
-API_KEY = 'FwLzNfy2zMccC5ApnWI1jbUfKos1SA1n';
-let articleLink =
-  'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${API_KEY}&fq=news_desk:("Business Day", "Business", "Entrepreneurs", "Financial", "Your Money")';
+API_KEY_NT = 'FwLzNfy2zMccC5ApnWI1jbUfKos1SA1n';
+let topStories = `https://api.nytimes.com/svc/topstories/v2/business.json?api-key=${API_KEY_NT}`;
 
 $.ajax({
-  url: articleLink,
+  url: topStories,
   method: 'GET',
-}).then(function (result) {
-  console.log(result);
-});
+})
+  .then(function (resp) {
+    console.log(resp);
+    // filter results by business only
+  })
+  .catch((err) => console.log(err));
+
+// this is a query for search
+let searchArticles = `https://api.nytimes.com/svc/search/v2/articlesearch.json?&fq=news_desk:("Business Day", "Business", "Entrepreneurs", "Financial", "Your Money")&api-key=${API_KEY_NT}`;
+
+$.ajax({
+  url: searchArticles,
+  method: 'GET',
+})
+  .then(function (resp) {
+    console.log(resp);
+    // filter results by business only
+  })
+  .catch((err) => console.log(err));
+
+// polygon.io
+API_KEY_STOCKS = '92bKvhEWQYOkEYm66Zp3bDSWLJJY5C5q';
+// TODO: get the array of stock names
+let stockName = 'AAPL';
+let yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+let today = moment().format('YYYY-MM-DD');
+let stockQuery = `https://api.polygon.io/v2/aggs/ticker/${stockName}/range/1/day/${yesterday}/${today}?adjusted=true&sort=asc&limit=120&apiKey=${API_KEY_STOCKS}`;
+
+$.ajax({
+  url: stockQuery,
+  method: 'GET',
+})
+  .then(function (resp) {
+    console.log(resp);
+  })
+  .catch((err) => console.log(err));
+
+let crypto = 'BTC';
+let currency = 'USD';
+let cryptoQuery = `https://api.polygon.io/v1/open-close/crypto/${crypto}/${currency}/${today}?adjusted=true&apiKey=${API_KEY_STOCKS}`;
+
+$.ajax({
+  url: cryptoQuery,
+  method: 'GET',
+})
+  .then(function (resp) {
+    console.log(resp);
+  })
+  .catch((err) => console.log(err));
